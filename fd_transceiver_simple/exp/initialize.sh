c7ca491ee9239bda9a9ea7e2a1b35381555889c7
Tingjun Chen <tc2668@columbia.edu>
2017-11-03 14:38:55 -0400

    Added topology.txt and grid console init script.

---
+++ fd_transceiver_simple/exp/initialize.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# This script flashes the baseline.ndz OS image in all platform nodes
+# NOTE:
+# Usage: ./initialize.sh <username>
+
+# Log into platform's main grid console
+ssh $1@console.grid.orbit-lab.org << 'END_CONSOLE_SSH'
+
+    NODEX=11
+    NODEY=10
+
+    # Turn the exp nodes off (hard)
+    echo "Turning the exp nodes off, please wait..."
+    omf tell -a offh -t node$NODEX-$NODEY
+
+    # Sleep for 60s for safety purposes
+    echo "Sleeping for 60s seconds, please wait..."
+    sleep 60
+
+    # Flash baseline-sdr.ndz to desired node
+    # NOTE: we set the flashing timeout to 1000s, as suggested by Ivan Seskar
+    echo "Flashing OS image in all nodes, please wait..."
+    omf load -i baseline-sdr.ndz -t node$NODEX-$NODEY -o 300
+
+    # Sleep for 60s for safety purposes (default was 120s)
+    echo "Sleeping for 60s seconds, please wait..."
+    sleep 60
+
+    # Turn the exp nodes on
+    echo "Turning the exp nodes on, please wait..."
+    omf tell -a on -t node$NODEX-$NODEY
+
+    # Farewell
+    echo "Initialization process finished."
+    echo "Use node"$NODEX"-"$NODEY"."
+    echo "Please allow some time for nodes to boot up and become sshable before moving to the next step."
+    echo
+
+    # Logout from console
+    exit
+
+END_CONSOLE_SSH
